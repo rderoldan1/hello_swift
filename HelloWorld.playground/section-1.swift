5ee21ae4a7707378a5658b431adfc8313fe0d900
Ruben Espinosa <rderoldan1@gmail.com>
2014-10-04 14:57:05 -0500

    First commit

---
+++ HelloWorld.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Constants and variables
+
+let myConstant = "Constant"
+var myVariable = "Variable"
+
+
+// Numbers
+
+24 + 2
+24 + Float(2)
+24 + Double(2)
+24 + 2.0
+
+
+var myFloat = Float(70)
+
+var myString: String
+myString = "Hello"
+
